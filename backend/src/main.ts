67ef008a297cf4f9ccda0634f244700497549297
israelConstruex <israelherrerae@gmail.com>
2026-03-12 18:51:23 -0500

    fix: especificar la dirección de enlace en el servidor al iniciar la aplicación

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -43,6 +43,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT ?? 4000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
